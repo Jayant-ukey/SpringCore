b6e91b429a52945821dd216e6b90aca51f905c0e
Jayant <ukeyjayant@gmail.com>
2024-08-16 21:50:09 +0530

    Used autowired annotaion before varible so we don't need setter methods

---
+++ 7_Annotaions/4_FullConfigurationUsingAnnotations/5_UsedAutowiredAnnotation/spring-common-annotations/src/main/java/com/seleniumexpress/college/Principal.java
@@ -0,0 +1,12 @@
+package com.seleniumexpress.college;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Principal {
+
+	public void principalInfo() {
+		System.out.println("Hi I am the Pincipal of the college");
+		System.out.println("and My name is James Bond");
+	}
+}
