c4e86fa3234eee4008e66d295a1e22d447fd9cae
Jayant <ukeyjayant@gmail.com>
2024-08-16 20:09:22 +0530

    Added example of using componenet scan but without passing bean id, which shows it take bydefault name of the class with first letter as small

---
+++ 7_Annotaions/2_WithoutXMl/WithoutPassingBeanIdInComponenet/spring-common-annotations/src/main/java/com/seleniumexpress/college/College.java
@@ -0,0 +1,11 @@
+package com.seleniumexpress.college;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class College {
+	
+	public void test() {
+		System.out.println("This method is  for testing purpose..");
+	}
+}
---
+++ 7_Annotaions/2_WithoutXMl/WithoutPassingBeanIdInComponenet/spring-common-annotations/src/main/java/com/seleniumexpress/college/CollegeConfig.java
@@ -0,0 +1,10 @@
+package com.seleniumexpress.college;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.seleniumexpress.college")
+public class CollegeConfig {
+
+}
