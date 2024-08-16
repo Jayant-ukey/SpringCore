cf325c9232d9b484068446967fb35610690fb276
Jayant <ukeyjayant@gmail.com>
2024-08-16 21:34:57 +0530

    Did complete setup using @Componenet annotaiton

---
+++ 7_Annotaions/4_FullConfigurationUsingAnnotations/4_UsingComponenetAnnotation/spring-common-annotations/src/main/java/com/seleniumexpress/college/College.java
@@ -0,0 +1,28 @@
+package com.seleniumexpress.college;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class College {
+	
+	@Autowired
+	private Principal principal;
+	
+	@Autowired
+	private Teacher teacher;
+		
+	public void setPrincipal(Principal principal) {
+		this.principal = principal;
+	}
+	
+	public void setTeacher(Teacher teacher) {
+		this.teacher = teacher;
+	}
+
+	public void test() {
+		principal.principalInfo();
+		teacher.teach();
+		System.out.println("This method is  for testing purpose..");
+	}
+}
