a616da1030766087fe015c07409269fcf9b37cf6
Jayant <ukeyjayant@gmail.com>
2024-08-16 22:01:56 +0530

    Used properties file to set value of college

---
+++ 7_Annotaions/5_UsingConfigClass/2_UsedPropertiesFIle/spring-common-annotations/src/main/java/com/seleniumexpress/college/CollegeConfig.java
@@ -0,0 +1,12 @@
+package com.seleniumexpress.college;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan("com.seleniumexpress.college")
+@PropertySource("college-info.properties")
+public class CollegeConfig {
+	
+}
